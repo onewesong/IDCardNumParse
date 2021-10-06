81c4c8442894e14a37a01b78fbfaa5caaaee4470
onewesong <onewesong@gmail.com>
2021-10-06 17:11:10 +0800

    first release

---
+++ precache-manifest.eecf35543c3434c7c5fdcb659a91986c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac4402f500a84852c436232efbbc0c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "963b9de0126d70040982",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c9841be1edd414b2756",
+    "url": "/static/js/2.886d49af.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.886d49af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "963b9de0126d70040982",
+    "url": "/static/js/main.5ee50106.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
